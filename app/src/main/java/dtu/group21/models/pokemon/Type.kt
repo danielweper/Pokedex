7f95e4ce39595b900d31f600aa414441c1507204
Daniel Weper Jensen <danielweper@gmail.com>
2023-10-09 11:17:40 +0200

    Added an enum for the types a Pokemon can be

---
+++ app/src/main/java/dtu/group21/models/pokemon/Type.kt
@@ -0,0 +1,20 @@
+enum class Type {
+    NORMAL,
+    FIRE,
+    WATER,
+    ELECTRIC,
+    GRASS,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DRAGON,
+    DARK,
+    STEEL,
+    FAIRY
+}
